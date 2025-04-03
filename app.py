69bfd76cc04d51943347de837867afcba72537e9
ishaan1234 <31759888+ishaan1234@users.noreply.github.com>
2025-04-03 20:41:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -238,4 +238,6 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    PORT = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
